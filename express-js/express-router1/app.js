24c827d295d175997aa4468e9495b60bbd890f7e
kimaya <kimaya@localhost.localdomain>
2020-10-13 10:59:57 +0530

    completed express js

--- express-js/express-router1/app.js
+++ express-js/express-router1/app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const shopRoutes = require('./routes/shop');
 const adminRoutes = require('./routes/admin');
 const path = require('path') // node js core module
-// const rootDir = require('./utils/path');
+const rootDir = require('./utils/path');
 const app = express();
 
 // to see the post data in req.body
@@ -15,7 +15,7 @@ app.use('/admin', adminRoutes);
 
 app.use((req,res,next)=>{
     // res.status(404).send('<h1>NOT FOUND !! ERROR 404</h1>');
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 app.listen(3000);
